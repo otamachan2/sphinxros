5a83b6623a5105d2b1a21ccfbf2c767488437772
Tamaki Nishino <otamachan@gmail.com>
2015-09-30 06:09:59 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -xv
 cd $(dirname $0)
 ls -al
 sudo apt-get update
-sudo apt-get install -f python-pip aptitude
-sudo aptitude install -f ros-indigo-*
+sudo apt-get install -f -y python-pip aptitude
+blacklists=(pepper naoqi nao romeo ipa-canopen shadow-robot)
+sudo apt-get install -f -y $(apt-cache -q search ros-indigo | cut -f 1 -d " " | grep -Ev $(IFS=\|; echo "${blacklists[*]}"))
 sudo pip install -r requirements.txt
 sudo pip install git+https://github.com/otamachan2/sphinxcontrib-ros.git
 ./scripts/generator.py indigo
