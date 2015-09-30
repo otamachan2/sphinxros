0d3cfbf17e940666a3641f6d06f047ceb7c250be
Tamaki Nishino <otamachan@gmail.com>
2015-10-01 06:41:45 +0900

    change return code and add all packages

--- get_debs.sh
+++ get_debs.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 # download
 sudo apt-get update -q
-#packages=$(apt-cache -q search ros-indigo | cut -f 1 -d " ")
-packages=$(apt-cache -q search ros-indigo | head -n 2 | cut -f 1 -d " ")
+packages=$(apt-cache -q search ros-indigo | cut -f 1 -d " ")
+#packages=$(apt-cache -q search ros-indigo | head -n 2 | cut -f 1 -d " ")
 cd debs
 apt-get download $packages
