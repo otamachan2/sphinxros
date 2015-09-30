b0b8221f70f740d99f725da0a129a8b0c7f53527
Tamaki Nishino <otamachan@gmail.com>
2015-10-01 05:48:54 +0900

    all packages

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ sudo apt-get update -q
 sudo apt-get install -f -y -q python-pip
 sudo pip install -r requirements.txt
 sudo pip install git+https://github.com/otamachan2/sphinxcontrib-ros.git
-packages=$(apt-cache -q search ros-indigo | head -n 2 | cut -f 1 -d " ")
+packages=$(apt-cache -q search ros-indigo | cut -f 1 -d " ")
+#packages=$(apt-cache -q search ros-indigo | head -n 2 | cut -f 1 -d " ")
 pushd debs; apt-get download $packages; popd
 for deb in `find debs -iname '*.deb'`; do
     echo $deb
