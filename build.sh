c5062ead55c1792866e7709060b46db73b096ef9
Tamaki Nishino <otamachan@gmail.com>
2015-09-30 23:01:56 +0900

    udpate

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@ sudo apt-get update
 sudo apt-get install -f -y python-pip
 blacklists=(pepper naoqi nao romeo ipa-canopen shadow-robot)
 packages=$(apt-cache -q search ros-indigo | cut -f 1 -d " " | grep -Ev $(IFS=\|; echo "${blacklists[*]}"))
-packages=$(apt-cache -q search ros-indigo | cut -f 1 -d " ")
+packages=$(apt-cache -q search ros-indigo | head -n 2 | cut -f 1 -d " ")
 mkdir debs
 pushd debs
 apt-get download $packages
-sudo dpkg --force-all -i *.deb
+for deb in `find . -iname '*.deb'`; do
+    sudo dpkg --force-all -i $deb
+done
 popd
 sudo pip install -r requirements.txt
 sudo pip install git+https://github.com/otamachan2/sphinxcontrib-ros.git
