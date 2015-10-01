9e25fb8a6dde780546e25ad2afec3ac691e64f64
Tamaki Nishino <otamachan@gmail.com>
2015-10-02 06:22:29 +0900

    wip

--- scripts/get_debs.sh
+++ scripts/get_debs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd $(dirname $0)/$1
+cd $1
 sudo apt-get update -q
 #packages=$(apt-cache -q search ros-${ROSDISTRO} | head -n 2 | cut -f 1 -d " ") # for test
 packages=$(apt-cache -q search ros-${ROSDISTRO} | cut -f 1 -d " ")
