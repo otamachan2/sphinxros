24e8c437a8a69b32448c2e5b969eccd48c8f9a86
Tamaki Nishino <otamachan@gmail.com>
2015-10-02 05:48:51 +0900

    change branch name

--- get_debs.sh
+++ get_debs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)/debs
 sudo apt-get update -q
-#packages=$(apt-cache -q search ros-indigo | head -n 2 | cut -f 1 -d " ") # for test
-packages=$(apt-cache -q search ros-indigo | cut -f 1 -d " ")
+#packages=$(apt-cache -q search ros-${ROSDISTRO} | head -n 2 | cut -f 1 -d " ") # for test
+packages=$(apt-cache -q search ros-${ROSDISTRO} | cut -f 1 -d " ")
 apt-get download $packages
